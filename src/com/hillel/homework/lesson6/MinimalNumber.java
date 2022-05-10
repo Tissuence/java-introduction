d8babb0e21429036a0d3e6931f3820e4c21b6736
Tissuence <amaranta345@gmail.com>
2022-05-10 21:55:10 +0300

    Add classwork lesson 6

---
+++ src/com/hillel/homework/lesson6/MinimalNumber.java
@@ -0,0 +1,16 @@
+package com.hillel.homework.lesson6;
+
+public class MinimalNumber {
+    public static void main(String[] args) {
+
+        /* Задано 3 случайных числа. Вывести минимальное. */
+        int a = 5;
+        int b = 45;
+        int c = -4;
+
+        int v = Math.min(a, b);
+        System.out.println(v);
+
+
+    }
+}
