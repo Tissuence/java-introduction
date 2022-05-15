b1260508003c96129233dfa5dac088ed19a784f8
Tissuence <amaranta345@gmail.com>
2022-05-15 13:48:57 +0300

    Add changes in homework lesson 6 (15.05.2022)

--- src/com/hillel/homework/lesson6/MinimalNumber.java
+++ src/com/hillel/homework/lesson6/MinimalNumber.java
@@ -7,26 +7,22 @@ import java.io.InputStreamReader;
 public class MinimalNumber {
     public static void main(String[] args) throws IOException {
 
-        /* Задано 3 случайных числа. Вывести минимальное. */
-
         BufferedReader reader = new BufferedReader ( new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
         int c = Integer.parseInt(reader.readLine());
 
-        whichMinimal(a,b,c);
+        System.out.println(" Minimal number is: " + whichMinimal(a,b,c));
 
     }
 
-    public static void whichMinimal(int a, int b, int c) {
+    public static int whichMinimal(int a, int b, int c) {
         if (a <= b && a <= c) {
-            System.out.println("Minimal number is " + a);
+            return a;
         } else if (b <= a && b <= c) {
-            System.out.println("Minimal number is " + b);
+            return b;
         } else {
-            System.out.println("Minimal number is " + c);
+            return c;
         }
-
-
     }
 }
