57bb0ec35140ffba438aab26e1ba887155de9a59
Tissuence <amaranta345@gmail.com>
2022-05-15 13:08:52 +0300

    Add classwork lesson 7 (15.05.2022)

---
+++ src/com/hillel/classwork/lesson7/Bomb.java
@@ -0,0 +1,23 @@
+package com.hillel.classwork.lesson7;
+
+public class Bomb {
+    public static void main(String[] args) throws InterruptedException {
+
+        int i = 10;
+        do {
+            System.out.println("До взрыва осталось " + i + " секунд...");
+            Thread.sleep(1000);
+            i--;
+
+            if (i == 5) {
+                break;
+            }
+        } while (i >= 0);
+
+        if (i == 0) {
+            System.out.println("Boom!");
+        } else {
+            System.out.println("Обезврежено!");
+        }
+    }
+}
---
+++ src/com/hillel/classwork/lesson7/For.java
@@ -0,0 +1,20 @@
+package com.hillel.classwork.lesson7;
+
+public class For {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 100; i = i + 2){
+            System.out.println("i = " + i );
+        }
+        for (int i = 100; i >= 1; i--){
+            System.out.println("i = " + i );
+        }
+        for (int i = 0; i < 100; i++) {
+        if (i % 4 == 0 || i % 5 == 0 ) {
+            System.out.println("i = " + i );
+        }
+        }
+    }
+
+}
