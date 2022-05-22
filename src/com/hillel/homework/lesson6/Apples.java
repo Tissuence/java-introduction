2107866b27dbc975922553be99243bd47c06c8e8
Tissuence <amaranta345@gmail.com>
2022-05-22 14:56:17 +0300

    Add changes in homework lesson 6 (22.05.2022)

--- src/com/hillel/homework/lesson6/Apples.java
+++ src/com/hillel/homework/lesson6/Apples.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 
 public class Apples {
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter quantity of apples:");
@@ -16,25 +15,27 @@ public class Apples {
         int capacityMediumJar = 3;
         int capacitySmallJar = 1;
 
+        int restApplesAfterBigJar = restOfApples(apples, capacityBigJar);
+        int restApplesAfterBMediumJar = restOfApples(restApplesAfterBigJar, capacityMediumJar);
+
         if (apples <= 0) {
             System.out.println("Incorrect data");
             return;
         }
         System.out.println(" In Big Jar: " + distributeApples(apples, capacityBigJar));
-        System.out.println(" In Medium Jar: " + distributeApples(restOfApples(apples, capacityBigJar), capacityMediumJar));
-        System.out.println(" In Small Jar: " + distributeApples(restOfApples(restOfApples(apples, capacityBigJar), capacityMediumJar), capacitySmallJar));
+        System.out.println(" In Medium Jar: " + distributeApples(restApplesAfterBigJar, capacityMediumJar));
+        System.out.println(" In Small Jar: " + distributeApples(restApplesAfterBMediumJar, capacitySmallJar));
 
     }
-
 
     public static int distributeApples(int apples, int capacityOfTheJar) {
         return apples / capacityOfTheJar;
     }
 
     public static int restOfApples(int apples, int capacityOfTheJar) {
-        return apples % capacityOfTheJar;
+        int restApples = apples % capacityOfTheJar;
+        return restApples;
     }
-
 
 }
 
