ca5f6c0377e76c65b72d042f9890330fb2ecc2c1
Tissuence <amaranta345@gmail.com>
2022-05-22 14:42:13 +0300

    Add changes in homework lesson 7 (22.05.2022)

--- src/com/hillel/homework/lesson7/CarWashingFor.java
+++ src/com/hillel/homework/lesson7/CarWashingFor.java
@@ -15,31 +15,29 @@ public class CarWashingFor {
             System.out.println("Incorrect data!");
 
         }
-        soapCar(quantityOfTheCars);
-        washCar(quantityOfTheCars);
-        dryCar(quantityOfTheCars);
+
+        for (int i = 0; i <= quantityOfTheCars; i++) {
+
+            soapCar(quantityOfTheCars, i);
+            washCar(quantityOfTheCars, i);
+            dryCar(quantityOfTheCars, i);
+
+        }
+
+        System.out.println("Already washed all cars!");
 
     }
 
-    public static void soapCar(int quantityOfTheCars) {
-        int i;
-        for (i = 1; i <= quantityOfTheCars; i++) {
-            System.out.println(" Wow, " + i + " cars successfully soaped!");
-        }
+    public static void soapCar(int quantityOfTheCars, int numberOfCar) {
+        System.out.println(" Wow, " + numberOfCar + " car successfully soaped!");
     }
 
-    public static void washCar(int quantityOfTheCars) {
-        int i;
-        for (i = 1; i <= quantityOfTheCars; i++) {
-            System.out.println(" Okay, " + i + " cars successfully washed!");
-        }
+    public static void washCar(int quantityOfTheCars, int numberOfCar) {
+        System.out.println(" Okay, " + numberOfCar + " car successfully washed!");
     }
 
-    public static void dryCar(int quantityOfTheCars) {
-        int i;
-        for (i = 1; i <= quantityOfTheCars; i++) {
-            System.out.println(" Great, " + i + " cars successfully dried!");
-        }
+    public static void dryCar(int quantityOfTheCars, int numberOfCar) {
+        System.out.println(" Great, " + numberOfCar + " car successfully dried!");
     }
 
 }
