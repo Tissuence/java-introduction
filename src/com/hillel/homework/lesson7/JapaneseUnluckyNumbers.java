8cb14055171610a7accc245a1803805c4e06f4c5
Tissuence <amaranta345@gmail.com>
2022-05-22 14:45:40 +0300

    Add changes in homework lesson 7 (22.05.2022)

--- src/com/hillel/homework/lesson7/JapaneseUnluckyNumbers.java
+++ src/com/hillel/homework/lesson7/JapaneseUnluckyNumbers.java
@@ -5,7 +5,7 @@ public class JapaneseUnluckyNumbers {
     public static void main(String[] args) {
         for (int i = 0; i <= 100; i++) {
 
-             if ((i - 4) % 10 == 0 || ((i - 9) % 10 == 0) || i / 10 == 4 || i / 10 == 9 ) {
+             if (i % 10 == 4 || i  % 10 == 9 || i / 10 == 4 || i / 10 == 9 ) {
                 continue;
             }
             System.out.println("i = " + i);
